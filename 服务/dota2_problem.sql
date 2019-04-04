e1253b69e8d28bdc66b082f531f0b3eb3d5cf6c6
weblxg6014 <45424315+weblxg6014@users.noreply.github.com>
2019-04-04 09:08:27 +0800

    Create dota2_problem.sql

---
+++ 服务/dota2_problem.sql
@@ -0,0 +1,16 @@
+USE dota2;
+CREATE TABLE dota_problem(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255),
+    problem_date DATE
+)
+
+INSERT INTO dota_problem VALUES(null,"如何找回与DOTA2账户绑定的完美通行证？");
+INSERT INTO dota_problem VALUES(null,"为什么我需要注册两个账号");
+INSERT INTO dota_problem VALUES(null,"如何注册DOTA2账号");
+INSERT INTO dota_problem VALUES(null,"如何绑定完美通行证");
+INSERT INTO dota_problem VALUES(null,"进入客户端后【运行DOTA2】按钮无法点击，怎么办？");
+INSERT INTO dota_problem VALUES(null,"DOTA2概率与结果公示信息");
+INSERT INTO dota_problem VALUES(null,"如何运行DOTA2？");
+INSERT INTO dota_problem VALUES(null,"忘记密码怎么办？");
+INSERT INTO dota_problem VALUES(null,"DOTA2新手攻略教程");
